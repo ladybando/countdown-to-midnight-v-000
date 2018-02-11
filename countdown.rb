01140713acc579ac0cdbba375ca87fae1efcb628
Ayana Bando <ayanaflax@yahoo.com>
2018-02-11 22:39:35 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown
   seconds = 0
-  while seconds < 10
+  until seconds == 10
     puts "#{number} SECOND(S)!"
   end
     return "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep
   seconds = 0
-  while seconds < 10
+  until seconds == 10
     puts "#{number} SECOND(S)!"
     sleep 1
   end
